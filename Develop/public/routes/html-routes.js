eb6f379a7068e0faa48190c99bc6d3bb22aeefbd
Kenlau94 <kenlauwork94@gmail.com>
2023-11-04 04:05:43 -0400

    fixing routing error -.-

--- Develop/public/routes/html-routes.js
+++ Develop/public/routes/html-routes.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
 const path = require("path");
 
-// more codeeeeeeeeeeeeeee????????????????????
+// final code for html????????????????????
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
+
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+});
 
 module.exports = router;
